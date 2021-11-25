b63a752805101f5f71ec0671097ecef7a4a50a5f
SanjanaKute <sanjanakute123@gmai.com>
2021-11-25 22:07:40 +0530

    Assignment Q1 and 2 added

---
+++ src/main/java/com/SpringSecurity/Practice/SpringSecurity/SpringSecurityApplication.java
@@ -0,0 +1,17 @@
+//Assignment Question1&2
+//localhost:8080/user
+//localhost:8080/admin
+
+package com.SpringSecurity.Practice.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
